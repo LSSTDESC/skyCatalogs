eeb01582f567d67e12a97e60d7f3481cd92fdb74
Joanne Bogart <jrb@slac.stanford.edu>
2022-01-18 18:44:59 -0800

    Add knots to the catalog

--- scripts/create_sc.py
+++ scripts/create_sc.py
@@ -31,8 +31,11 @@ parser.add_argument('--output-dir', help='directory for output files',
 parser.add_argument('--sed-subdir', help='subdirectory to prepend to paths of galaxy SEDs as written to the sky catalog', default='galaxyTopHatSED')
 parser.add_argument('--verbose', help='print more output if true',
                     action='store_true')
-parser.add_argument('--galaxy-magnitude-cut', default=None,
-                    help='If supplied exclude galaxies with magnitude above')
+parser.add_argument('--galaxy-magnitude-cut', default=None, type=float,
+                    help='If supplied exclude galaxies with r-magnitude above')
+parser.add_argument('--knots-magnitude-cut', default=27.0, type=float,
+                    help='Galaxies with i-magnitude above this cut get no knots')
+parser.add_argument('--no-knots', action='store_true', help='If supplied omit knots component. Default is False')
 
 args = parser.parse_args()
 
@@ -46,7 +49,9 @@ parts = args.pixels
 creator = CatalogCreator(parts, area_partition, output_dir=output_dir,
                          verbose=args.verbose,
                          mag_cut=args.galaxy_magnitude_cut,
-                         sed_subdir=args.sed_subdir)
+                         sed_subdir=args.sed_subdir,
+                         knots_mag_cut=args.knots_magnitude_cut,
+                         knots=(not args.no_knots))
 print('Starting with healpix pixel ', parts[0])
 if not args.no_galaxies:
     print("Creating galaxy catalogs")
