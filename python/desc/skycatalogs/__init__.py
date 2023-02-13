bf94e6772b2662ad5320a8de2e544e09fc71581b
Joanne Bogart <jrb@slac.stanford.edu>
2023-02-13 11:42:00 -0800

    use pyproject.toml

--- python/desc/skycatalogs/__init__.py
+++ python/desc/skycatalogs/__init__.py
@@ -1,3 +1,11 @@
-from ._version import *
+##from ._version import *
+try:
+    # For Python >= 3.8
+    from importlib import metadata
+except ImportError:
+    # For Python < 3.8
+    import importlib_metadata as metadata
+
+__version__ = metadata.version("mydescpackage")
 from .skyCatalogs import *
 from .catalog_creator import *
