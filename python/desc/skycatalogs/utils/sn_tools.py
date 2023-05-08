53a243a90c33202cc734b044897bda81fd37ce0b
Joanne Bogart <jrb@slac.stanford.edu>
2023-05-08 16:28:18 -0700

    prepend 0 bins to lookup table to avoid galsim error for red sources

--- python/desc/skycatalogs/utils/sn_tools.py
+++ python/desc/skycatalogs/utils/sn_tools.py
@@ -19,14 +19,23 @@ class SNObject(sncosmo.Model):
                          effect_names=['host', 'mw'],
                          effect_frames=['rest', 'obs'])
         self.set(mwebv=0., hostebv=0.)
+        self.redshift = 0
         if params:
             self.set(**params)
+            self.redshift = params['z']
 
     def get_sed(self, mjd, npts=1000):
         """
         Return the SED in the observer frame at the requested time.
         """
         wl = np.linspace(self.minwave(), self.maxwave(), npts)
+
+        # prepend 0 bins
+        n_bins = int(self.minwave()) - 1
+        pre_wl = [float(i) for i in range(n_bins)]
+        pre_val = [0.0 for i in range(n_bins)]
         flambda = self.flux(mjd, wl)
-        lut = galsim.LookupTable(wl, flambda)
+        wl = np.insert(wl, 0, pre_wl)
+        flambda = np.insert(flambda, 0, pre_val)
+        lut = galsim.LookupTable(wl, flambda, interpolant='linear')
         return galsim.SED(lut, wave_type=u.Angstrom, flux_type='flambda')
