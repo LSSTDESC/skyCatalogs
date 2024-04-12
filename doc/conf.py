6a249fecb23ae93a7759744212af4d6def453907
Joanne Bogart <jrb@slac.stanford.edu>
2024-04-12 15:46:43 -0700

    Start to produce package documentation and machinery to generate and publish

---
+++ doc/conf.py
@@ -0,0 +1,72 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+
+# Add top-level directory to path so python modules can be referred to
+# using the normal relative path
+import os
+import sys
+import importlib.util
+sys.path.insert(0, os.path.abspath('..'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'skyCatalogs'
+copyright = '2020-2024, LSSTDESC'
+author = 'LSSTDESC'
+
+# The full version, including alpha/beta/rc tags
+# release = '1.7.0rc2'
+# Use load_skycatalogs_version() to determine release dynamically
+
+def load_skycatalogs_version():
+    """Extract version of skyCatalogs without importing the whole module"""
+
+    spec = importlib.util.spec_from_file_location(
+        "skycatalogs_version",
+        os.path.join(os.path.dirname(__file__), "..", "skyCatalogs",
+                     "_version.py"),
+    )
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module    
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc"
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+skycatalogs_version = load_skycatalogs_version()
+release = skycatalogs_version.__version__
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ doc/index.rst
@@ -0,0 +1,31 @@
+.. skyCatalogs documentation master file, created by
+   sphinx-quickstart on Fri Apr 12 13:47:18 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+skyCatalogs: provides API for simulated object catalogs
+=======================================================
+skyCatalogs both creates simulated object catalogs (from precursor catalogs)
+and provides a uniform API to the catalogs it makes and to certain other
+catalogs. These catalogs may be used as inputs to image simulation (originally
+only for the Rubin Observatory; since also used in simulations for the
+Roman Obseratory) and as truth catalogs.
+
+This documentation covers installation and usage instructions for the package.
+
+The source code of *skyCatalogs* is hosted at
+https://github.com/LSSTDESC/skyCatalogs.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
