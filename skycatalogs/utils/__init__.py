57e1a7275cb79b40f87d7b295cceabaf6e1761a1
Joanne Bogart <jrb@slac.stanford.edu>
2024-04-16 16:39:48 -0700

    eliminate unnecessary import

--- skycatalogs/utils/__init__.py
+++ skycatalogs/utils/__init__.py
@@ -4,6 +4,6 @@ from .translate_utils import *
 from .exceptions import *
 from .parquet_schema_utils import *
 from .sed_tools import *
-from .sn_tools import *
+# from .sn_tools import *
 from .shapes import *
 from .creator_utils import *
