88d6a01ca758c428753bda32c6746ccacfeec30e
Joanne Bogart <jrb@slac.stanford.edu>
2024-12-04 14:36:11 -0800

    one-line bug fix

--- skycatalogs/objects/base_object.py
+++ skycatalogs/objects/base_object.py
@@ -316,7 +316,7 @@ class BaseObject(object):
         Use supplied sed if there is one
         """
 
-        if not sed:
+        if sed is None:
             sed = self.get_total_observer_sed(mjd=mjd)
 
         if sed is None:
