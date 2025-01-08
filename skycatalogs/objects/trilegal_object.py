1451999c4c80174cabf44392e5413355bd9ac8fc
Joanne Bogart <jrb@slac.stanford.edu>
2025-01-07 17:16:10 -0800

    Normalize SED after applying extinction Eliminate commented-out code. Functionality is handled within sed_tools.py

--- skycatalogs/objects/trilegal_object.py
+++ skycatalogs/objects/trilegal_object.py
@@ -28,14 +28,19 @@ class TrilegalObject(BaseObject):
         return {'this_object': galsim.DeltaFunction(gsparams=gsparams)}
 
     def get_observer_sed_component(self, component, mjd=None):
+        '''
+        Apply extinction, normalize
+        '''
         sed = self._get_sed(mjd=mjd)
 
         if sed is not None:
             sed = self._apply_component_extinction(sed)
+            imag = self.get_native_attribute('imag')
+            sed = sed.withMagnitude(imag, super().lsst_bandpasses['i'])
         return sed
 
     def _get_dust(self):
-        """Return the Av, Rv parameters for internal and Milky Way extinction."""
+        "Return the Av, Rv parameters for internal and Milky Way extinction."
         internal_av = 0
         internal_rv = 1.
 
@@ -71,84 +76,12 @@ class TrilegalCollection(ObjectCollection):
                          region=region, mjd=mjd, mask=mask,
                          readers=readers, row_group=row_group)
 
-        # Assume for now SED files, if any, are in same directory as
-        # parquet files. Make a SED reader which will manage SED data
-        # for this collection
+        # See also classes TrilegalSedFactory, TrilegalSedFile, _SEDBatch in
+        # sed_tools.py
+
 
         # Is this necessary? Probably not
         our_config = self.sky_catalog._config['object_types'][object_type]
-
-        # Instead of a _TrilegalSEDReader, we should have reference
-        # to a TrilegalSedFactory
-
-        # self._sed_reader = _TrilegalSEDReader(hp, our_config,
-        #                                       sky_catalog._cat_dir)
-
-
-# See instead class TrilegalSedFactory, TrilegalSedFile, _SEDBatch in
-# sed_tools.py
-
-# class _SEDBatch:
-#     def __init__(self, batch_name, hdf_file):
-#         self._batch_name = batch_name
-#         self._batch = hdf_file['batches'][batch_name]
-#         ids = self._batch['id']
-#         self._spectra = None
-#         self._hdf_file = hdf_file
-
-#         # Format for id is <truth_name>_hp<hp_id>_<count>
-#         # where <count> is the index of this source within the healpixel
-#         first_id = ids[0].decode()
-#         cmps = first_id.split('_')
-#         self._min_hp_ix = int(cmps[-1])
-#         # last_id = self._ids[-1].decode()
-#         # cmps = last_id.split('_')
-#         # self._max_hp_ix = int(cmps[-1])
-#         self._max_hp_ix = self._min_hp_ix + len(ids) - 1
-
-#     def get_sed_row(self, id):
-#         cmps = id.split('_')
-#         id_ix = int(cmps[-1])
-#         if (id_ix < self._min_hp_ix) or (id_ix > self._max_hp_ix):
-#             return None         # Not in this batch
-
-#         if not self._spectra:
-#             self._spectra = self._batch['spectra']
-
-#         return self._spectra[id_ix - self._min_hp_ix]
-
-
-# class _TrilegalSEDReader:
-#     def __init__(self, hp, object_type_config, cat_dir):
-#         tmpl = object_type_config.get('sed_file_template', None)
-#         self._hp = hp
-#         if not tmpl:
-#             raise Exception('Bad config file for Trilegal')
-#         self._fpath = os.path.join(cat_dir,
-#                                    tmpl.replace('(?P<healpix>\\d+)', str(hp)))
-#         self._file = h5py.File(self._fpath)
-#         self._batches = []
-#         self._wl = None
-
-#         # init the batchs
-#         for b in h5py.File['batches']:
-#             self._batches.append(_SEDBatch(b, self._file))
-
-#     def get_sed_row(self, id):
-#         cmps = id.split('_')
-#         if cmps[-2] != f'hp{self._hp}':
-#             raise Exception(f'This source is not in hp {self._hp}')
-#         for b in self._batches:
-#             sed_row = b.get_sed_row(id)
-#             if sed_row:
-#                 return sed_row
-#         # Should have warning log message here
-#         print(f'No SED for id {id}')
-#         return None
-
-#     def get_wl_axis(self):
-#         if not self._wl:
-#             self._wl = self._File['wl_axis']
 
 
 class TrilegalConfigFragment(BaseConfigFragment):
